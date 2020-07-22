fbe2617cc627e483fa6177a9460f383fc5747657
Pedro Goncalves <pedro.a.goncalves@gmail.com>
2020-07-22 22:03:52 +0800

    First testing scenario working with BDD behave tool

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
-clean: 
-	rmdir /s __pycache__
-
-init:
-	pip install -r requirements.txt
-
+PY=python -m py_compile
+.PHONY:
+    all
+    test
+    install
+    compile
+    clean
+#all:
+#    @make test
+#    @make install
 test:
-	behave
+    behave
+install:
+    pip install -r requirements.txt
+compile:
+    $(PY) add_expense.py
+clean: 
+	#rmdir /s /q .\__pycache__
+	del /q .\__pycache__\*.*
+	rmdir .\__pycache__
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,4 +1,5 @@
 from behave import *
+import subprocess
 
 @given('we have add_expense installed')
 def step_impl(context):
@@ -6,10 +7,16 @@ def step_impl(context):
 
 @when('we call add_expense without any parameters')
 def step_impl(context):
-	# TO DO
-	assert False 
-
+	#stream = os.popen('add_expense.py')
+	stream = subprocess.Popen("add_expense.py", shell=True, bufsize=0, stdout=subprocess.PIPE).stdout
+	output = stream.read()
+	context.cli_output = output.decode("utf-8") 
+	print(context.cli_output)
+	pass
+ 
 @then('add_expense should output the usage instructions')
-def step_impl(context):
-	# TO DO
-	assert False 
+def step_impl(context):	
+	if(context.cli_output.find('Usage') > -1):
+		assert True
+	else:
+		assert False
