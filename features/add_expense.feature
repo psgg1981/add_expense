7f3bfe5e4e22f5b5c04be6fa0908386d42b47d24
Pedro Goncalves <pedro.a.goncalves@gmail.com>
2020-07-23 14:28:38 +0800

    Added BDD scenarios for help command line call

--- features/add_expense.feature
+++ features/add_expense.feature
@@ -1,6 +1,16 @@
-Feature: simple program command line call with no parameters
+Feature: simple command line documentation usage
 
-	Scenario: run without any parameters 
+	Scenario: run without any parameters
 		Given we have add_expense installed
 		 When we call add_expense without any parameters
 		 Then add_expense should output the usage instructions
+
+	Scenario: run with help shorthand parameter
+		Given we have add_expense installed and will be testing the help shorthand parameter
+		 When we call add_expense with help parameter "-h"
+		 Then add_expense should output the instructions keyword "Usage"
+
+	Scenario: run with help parameter full label
+		Given we have add_expense installed and will be testing the help full spec parameter
+		 When we call add_expense with help full label parameter "--help"
+		 Then add_expense should output the instructions keyword "Usage" too
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,22 +1,65 @@
-from behave import *
+from behave import given, when, then
 import subprocess
 
-@given('we have add_expense installed')
+# Scenario: run without any parameters
+@given(u'we have add_expense installed')
 def step_impl(context):
 	pass
 
-@when('we call add_expense without any parameters')
+@when(u'we call add_expense without any parameters')
 def step_impl(context):
-	#stream = os.popen('add_expense.py')
 	stream = subprocess.Popen("add_expense.py", shell=True, bufsize=0, stdout=subprocess.PIPE).stdout
 	output = stream.read()
 	context.cli_output = output.decode("utf-8") 
-	print(context.cli_output)
+	#DEBUG print(context.cli_output)
 	pass
  
-@then('add_expense should output the usage instructions')
+@then(u'add_expense should output the usage instructions')
 def step_impl(context):	
 	if(context.cli_output.find('Usage') > -1):
 		assert True
 	else:
 		assert False
+
+
+# Scenario: run without any help parameter
+@given(u'we have add_expense installed and will be testing the help shorthand parameter')
+def step_impl(context):
+	pass
+
+@when(u'we call add_expense with help parameter "{parameter}"')
+def step_impl(context, parameter):
+	#DEBUG print("add_expense.py " + parameter) 
+	stream = subprocess.Popen("add_expense.py " + parameter, shell=True, bufsize=0, stdout=subprocess.PIPE).stdout
+	output = stream.read()
+	context.cli_output = output.decode("utf-8") 
+	#DEBUG print(context.cli_output)
+	pass
+
+@then(u'add_expense should output the instructions keyword "{keyword}"')
+def step_impl(context, keyword):	
+	#DEBUG print("keyword: " + keyword)
+	#DEBUG print("context.cli_output: " + context.cli_output)
+	#DEBUG print("test: " + str(context.cli_output.find(keyword) > -1))
+	if(context.cli_output.find(keyword) > -1):
+		assert True
+	else:
+		assert False
+
+
+# Scenario: run with help parameter --help
+@given(u'we have add_expense installed and will be testing the help full spec parameter')
+def step_impl(context):
+	pass
+
+@when(u'we call add_expense with help full label parameter "{parameter}"')
+def step_impl(context, parameter):
+	print('parameter: ' + parameter)
+	string_to_parse = u'When we call add_expense with help parameter "{new_input}"'.format(new_input=parameter)
+	print('string_to_parse: ' + string_to_parse)
+	context.execute_steps(string_to_parse)
+		 
+@then(u'add_expense should output the instructions keyword "{keyword}" too')
+def step_impl(context, keyword):	
+	print('keyword: ' + keyword)
+	context.execute_steps(u'Then add_expense should output the instructions keyword "{0}"'.format(keyword))
