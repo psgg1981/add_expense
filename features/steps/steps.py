cb51b6601966127fd1097ff9e6df9001bdd5a7b5
psgg1981 <pedro.a.goncalves@gmail.com>
2020-07-25 11:02:30 +0800

    Added logging module and swapped all print() functions with logging.debug(); also prefixed the system process command line executing add_expense with python

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,7 +1,7 @@
 from behave import given, when, then
 from hamcrest import assert_that, has_item
 import subprocess
-import os
+import os, logging
 
 # ##################################################
 # Feature: simple command line documentation usage
@@ -14,10 +14,10 @@ def step_impl(context):
 
 @when(u'we call add_expense without any parameters')
 def step_impl(context):
-	stream = subprocess.Popen("add_expense.py", shell=True, bufsize=0, stdout=subprocess.PIPE).stdout
+	stream = subprocess.Popen("python add_expense.py", shell=True, bufsize=0, stdout=subprocess.PIPE).stdout
 	output = stream.read()
 	context.cli_output = output.decode("utf-8") 
-	#DEBUG print(context.cli_output)
+	logging.debug(context.cli_output)
 	pass
  
 @then(u'add_expense should output the usage instructions')
@@ -35,18 +35,18 @@ def step_impl(context):
 
 @when(u'we call add_expense with help parameter "{parameter}"')
 def step_impl(context, parameter):
-	#DEBUG print("add_expense.py " + parameter) 
-	stream = subprocess.Popen("add_expense.py " + parameter, shell=True, bufsize=0, stdout=subprocess.PIPE).stdout
+	logging.debug("add_expense.py " + parameter) 
+	stream = subprocess.Popen("python add_expense.py " + parameter, shell=True, bufsize=0, stdout=subprocess.PIPE).stdout
 	output = stream.read()
 	context.cli_output = output.decode("utf-8") 
-	#DEBUG print(context.cli_output)
+	logging.debug(context.cli_output)
 	pass
 
 @then(u'add_expense should output the instructions keyword "{keyword}"')
 def step_impl(context, keyword):	
-	#DEBUG print("keyword: " + keyword)
-	#DEBUG print("context.cli_output: " + context.cli_output)
-	#DEBUG print("test: " + str(context.cli_output.find(keyword) > -1))
+	logging.debug("keyword: " + keyword)
+	logging.debug("context.cli_output: " + context.cli_output)
+	logging.debug("test: " + str(context.cli_output.find(keyword) > -1))
 	if(context.cli_output.find(keyword) > -1):
 		assert True
 	else:
@@ -60,12 +60,12 @@ def step_impl(context):
 
 @when(u'we call add_expense with help full label parameter "{parameter}"')
 def step_impl(context, parameter):
-	print('parameter: ' + parameter)
+	logging.debug('parameter: ' + parameter)
 	string_to_parse = u'When we call add_expense with help parameter "{new_input}"'.format(new_input=parameter)
-	print('string_to_parse: ' + string_to_parse)
+	logging.debug('string_to_parse: ' + string_to_parse)
 	context.execute_steps(string_to_parse)
 		 
 @then(u'add_expense should output the instructions keyword "{keyword}" too')
 def step_impl(context, keyword):	
-	print('keyword: ' + keyword)
+	logging.debug('keyword: ' + keyword)
 	context.execute_steps(u'Then add_expense should output the instructions keyword "{0}"'.format(keyword))
